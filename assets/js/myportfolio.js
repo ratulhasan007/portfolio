835df1ec7f43fe7cd7245d5e60160110d8bc2471
ratulhasan007 <112752297+ratulhasan007@users.noreply.github.com>
2022-09-09 09:35:22 +0600

    Update myportfolio.js

--- assets/js/myportfolio.js
+++ assets/js/myportfolio.js
@@ -1,5 +1,6 @@
 
 
+
 /* ===== typing animation ===== */
 
 var typed = new Typed(".typing",{
@@ -27,3 +28,65 @@ $(window).scroll(function() {
     }
 
     }).scroll();
+
+
+    
+    $(window).scroll(function(){
+    
+        $('.zoom-in').each(function(){        
+            var zoomIn = 1, zoomOut = 0;
+            
+            if(isScrolledIntoView($(this))){        
+                $(this).css({
+                    '-webkit-transform': 'scale(' + zoomIn + ')',
+                    'transform': 'scale(' + zoomIn + ')'
+                });
+            }
+            
+        });
+
+        //Fade-in
+        $('.fade-in').each(function(){        
+        
+        if(isScrolledIntoView($(this))){        
+         $(this).css({
+          'opacity':1,
+           'visibility':'visible'
+           });
+         }
+        });
+    });
+    
+    
+    function isScrolledIntoView(elem){
+        var $elem = $(elem);
+        var $window = $(window);
+      
+        var docViewTop = $window.scrollTop();
+        var docViewBottom = docViewTop + $window.height();
+      
+        var elemTop = $elem.offset().top;
+        var elemBottom = elemTop + $elem.height();
+      
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+
+    const body = document.body,
+      jsScroll = document.getElementsByClassName('js-scroll')[0],
+      height = jsScroll.getBoundingClientRect().height - 1,
+      speed = 0.07
+
+        var offset = 0
+
+        body.style.height = Math.floor(height) + "px"
+
+        function smoothScroll() {
+            offset += (window.pageYOffset - offset) * speed
+            
+            var scroll = "translateY(-" + offset + "px) translateZ(0)"
+            jsScroll.style.transform = scroll
+            
+            raf = requestAnimationFrame(smoothScroll)
+        }
+        smoothScroll()
